02da942306689f8ca06937a27e34f978833a7ef6
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-02-20 15:42:17 -0300

    Prática

---
+++ .Net/Práticas/bytebank/bytebank/Program.cs
@@ -0,0 +1,10 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+
+contaDoAndre.titular = "André Silva";
+contaDoAndre.conta = "1010-X";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Titular: " + contaDoAndre.titular);

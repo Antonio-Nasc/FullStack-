767a4534c51363b74750ecfabe62a4f03034bec6
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-02-22 20:40:01 -0300

    PRÁTICA
    Feito acréscimos dos métodos sacar, depositar e transferir. Também foi criado uma nova instância para a utilização do método transferir.

--- .Net/Práticas/bytebank/bytebank/Program.cs
+++ .Net/Práticas/bytebank/bytebank/Program.cs
@@ -7,4 +7,28 @@ contaDoAndre.conta = "1010-X";
 contaDoAndre.numero_agencia = 15;
 contaDoAndre.saldo = 100;
 
-Console.WriteLine("Titular: " + contaDoAndre.titular);
+Console.WriteLine("Saldo da conta do André é de " + contaDoAndre.saldo);
+
+ContaCorrente contaDaMaria =new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.conta = "1010-5";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.saldo = 350;
+
+Console.WriteLine("Saldo da conta da Maria é de " + contaDaMaria.saldo);
+
+contaDoAndre.Transferir(50, contaDaMaria);
+Console.WriteLine("Saldo da conta do André é de " + contaDoAndre.saldo);
+Console.WriteLine("Saldo da conta da Maria é de " + contaDaMaria.saldo);
+
+/*contaDoAndre.Depositar(100);
+Console.WriteLine("Saldo da conta do André é de " + contaDoAndre.saldo);
+
+if(contaDoAndre.Sacar(300) == true)
+{
+    Console.WriteLine("Saldo da conta do André pós-saque é de " + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente.");
+}*/

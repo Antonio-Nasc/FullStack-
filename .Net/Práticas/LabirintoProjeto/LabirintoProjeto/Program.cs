13864148d96f748982e13266fefc1d761ee7a43a
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-02-24 20:12:15 -0300

    Projeto labirinto

---
+++ .Net/Práticas/LabirintoProjeto/LabirintoProjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+
+string titulo = "Labirinto";
+Console.Title = titulo;
+
+string[,] labirinto = new string[4, 5]
+{
+    {"1","0","0","0","1" },
+    {"0","0","1","0","1" },
+    {"1","1","1","0","F" },
+    {"1","1","1","1","1" }
+};
+
+string player = "X";
+labirinto[1, 0] = player;
+
+for (int linha = 0; linha < 4; linha++)
+{
+    for(int coluna = 0; coluna < 5; coluna++)
+    {
+        Console.Write(labirinto[linha, coluna] + " ");
+    }
+    Console.WriteLine();
+}

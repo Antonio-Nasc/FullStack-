2d5d4458ef0ca1f323ba0edd4a47044dba1abf7b
José Antônio Do Nascimento Neto <joseeantonioo2000@gmail.com>
2023-03-06 13:23:54 -0300

    Prática

---
+++ .Net/Práticas/bytebank_ADM/bytebank_ADM/Program.cs
@@ -0,0 +1,16 @@
+﻿using bytebank_ADM.Funcionarios;
+
+Funcionario funcionario = new Funcionario();
+funcionario.Nome = "José Nascimento";
+funcionario.Cpf = "123456789";
+funcionario.Salario = 3500;
+Console.WriteLine(funcionario.Nome);
+Console.WriteLine(funcionario.Salario);
+Console.WriteLine(funcionario.GetBonificacao());
+
+Diretor Jorge = new Diretor();
+Jorge.Nome = "Jorge Oliveira";
+Jorge.Cpf = "44444444";
+Jorge.Salario = 6000;
+Console.WriteLine(Jorge.Nome);
+Console.WriteLine(Jorge.GetBonificacao());
